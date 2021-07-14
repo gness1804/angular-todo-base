31e01000df656540f6813bf58618c7bb8f8fc595
tubatoothpaste <itskylerobertson@gmail.com>
2021-07-14 15:02:13 -0400

    alternate confirm

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -25,10 +25,17 @@ export class AppComponent {
     const dialogRef = this.dialog.open(ConfirmationDialogComponent);
 
     dialogRef.afterClosed().pipe(
+      // this takes the result of the dialog button press, and halts the process if the response wasn't "true".
+      // the commented code below does the same with just a little more code.
       filter(Boolean)
     ).subscribe(() => {
       this.tasks.splice(this.tasks.indexOf(task), 1);
     });
+
+    // dialogRef.afterClosed().subscribe((confirmed: boolean) => {
+    //   if (!confirmed) return;
+    //   this.tasks.splice(this.tasks.indexOf(task), 1);
+    // });
   }
 
 }
