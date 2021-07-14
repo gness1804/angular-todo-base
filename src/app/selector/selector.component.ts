1ea24a2ea97777ce3346dbf816c56ab2d64a5767
gness1804 <graham.nessler@gmail.com>
2021-07-14 17:54:22 -0500

    add superheros select

--- src/app/selector/selector.component.ts
+++ src/app/selector/selector.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Superhero {
+  id: number;
+  name: string;
+}
+
 @Component({
   selector: 'app-selector',
   templateUrl: './selector.component.html',
@@ -8,6 +13,16 @@ import { Component, OnInit } from '@angular/core';
 export class SelectorComponent implements OnInit {
 
   constructor() { }
+
+  superheros: Superhero[] = [
+    { id: 1, name: 'Black Widow'},
+    { id: 2, name: 'Captain America'},
+    { id: 3, name: 'Iron Man'},
+    { id: 4, name: 'Incredible Hulk'},
+    { id: 5, name: 'Spider Man'},
+  ];
+
+  selected: Superhero = this.superheros[0];
 
   ngOnInit(): void {
   }
